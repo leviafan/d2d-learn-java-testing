0273c9e563c7168501c0999fff549432f88bc8f5
Timur N <leviafan89@mail.ru>
2017-10-31 20:37:25 +0500

    Add Field.java - class for game field.

---
+++ src/main/java/net/_900913/d2d/Field.java
@@ -0,0 +1,14 @@
+package net._900913.d2d;
+
+import net._900913.d2d.block.Cell;
+
+import java.util.ArrayList;
+
+public class Field {
+    private int width;
+    private int length;
+    ArrayList<ArrayList<Cell>> field = new ArrayList<ArrayList<Cell>>();
+    Field(int width, int length) {
+        
+    }
+}
